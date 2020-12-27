f9778c96e77f6b12eeb8af004850549f133634a5
Emre <emrekurtt1655@gmail.com>
2020-12-27 04:24:31 +0100

    finish context api

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,5 +1,7 @@
-const initialState = {
-  todos: [],
+export const initialState = {
+  todos: [
+    
+  ],
 };
 
 const reducer = (state, action) => {
@@ -7,7 +9,44 @@ const reducer = (state, action) => {
     case "ADD_TODO":
       return {
         ...state,
+        todos: [action.payload, ...state.todos],
       };
+
+    case "REMOVE_TODO":
+      return {
+        ...state,
+        todos: [...state.todos].filter((todo) => todo.id !== action.payload),
+      };
+
+      case 'UPDATE_TODO':
+        return {
+          ...state,
+          todos: state.todos.map((todo) => {
+            if (todo.id !== action.payload.todoId) {
+              return todo;
+            }
+  
+            return {
+              ...todo,
+              content: action.payload.newValue,
+            };
+          }),
+        };
+
+    case "COMPLETE_TODO":
+      return {
+        ...state,
+        todos: state.todos.map((todo)=>{
+          if(todo.id !== action.payload) {
+            return todo;
+          }
+          return {
+            ...todo,
+            isCompleted: !todo.isCompleted
+          }
+        })
+      };
+
     default:
       return {
         ...state,
