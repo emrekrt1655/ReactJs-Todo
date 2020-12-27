799a83e0461bf3487a0b9a768e3fce4b3adf16ed
Emre <emrekurtt1655@gmail.com>
2020-12-27 22:46:18 +0100

    add count todos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react';
-import TodoList from './components/TodoList';
-import { useTodoLayerValue } from './context/TodoContext';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import TodoList from "./components/TodoList";
+import { useTodoLayerValue } from "./context/TodoContext";
+import "./App.css";
 
 const App = () => {
   const [{ todos }, dispatch] = useTodoLayerValue();
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState("");
 
   const inputRef = useRef(null);
 
   useEffect(() => {
     inputRef.current.focus();
   }, []);
+
+  const countTodo = [];
+  const countDone = [];
+  todos &&
+    todos.map((todo) => {
+      !todo?.isCompleted && countTodo.push(todo);
+      todo?.isCompleted && countDone.push(todo);
+    });
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -24,16 +32,24 @@ const App = () => {
       };
 
       dispatch({
-        type: 'ADD_TODO',
+        type: "ADD_TODO",
         payload: newTodo,
       });
 
-      setContent('');
+      setContent("");
     }
   };
 
   return (
     <div className="container">
+      <div className='countPart'>
+        <h1 className="count">
+          <span>Todos :</span> {countTodo.length}{" "}
+        </h1>
+        <h1 className="count">
+          <span>Done :</span> {countDone.length}{" "}
+        </h1>
+      </div>
       <form onSubmit={handleSubmit} className="todo-form">
         <input
           type="text"
@@ -46,6 +62,7 @@ const App = () => {
 
         <button className="todo-button">Add</button>
       </form>
+
       <TodoList todos={todos} />
     </div>
   );
