e6c6257f749e0a6b7ea2f5a2c79515b1e1cad04a
Emre <emrekurtt1655@gmail.com>
2020-12-27 04:26:01 +0100

    add useRef and finish app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import TodoList from './components/TodoList';
+import { useTodoLayerValue } from './context/TodoContext';
+import './App.css';
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [{ todos }, dispatch] = useTodoLayerValue();
+  const [content, setContent] = useState('');
 
-export default App
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (content) {
+      const newTodo = {
+        id: Math.floor(Math.random() * 39399393),
+        content,
+        isCompleted: false,
+      };
+
+      dispatch({
+        type: 'ADD_TODO',
+        payload: newTodo,
+      });
+
+      setContent('');
+    }
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="todo-form">
+        <input
+          type="text"
+          value={content}
+          className="todo-input"
+          placeholder="What Are you plaining?"
+          ref={inputRef}
+          onChange={(event) => setContent(event.target.value)}
+        />
+
+        <button className="todo-button">Add</button>
+      </form>
+      <TodoList todos={todos} />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { TodoLayer } from "./context/TodoContext";
+import reducer, { initialState } from "./context/reducer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </TodoLayer>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
